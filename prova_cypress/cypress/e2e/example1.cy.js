62e52af26bcc8b65e521eb5527382c70f2482a29
annac@NITRO_ACR <annaclararenno@gmail.com>
2023-12-02 22:40:14 -0300

    Fazendo primeiro caso de teste cypress, teste onde deposita 100 reais no User do Harry Potter

--- prova_cypress/cypress/e2e/example1.cy.js
+++ prova_cypress/cypress/e2e/example1.cy.js
@@ -1,5 +1,34 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+/// <reference types = "cypress"/>
+
+describe('Criando cenario de teste para o site globalsqa',()=>{
+
+  it.skip('Caso de teste: Fazer deposito de 100 reais usando o user do Harry Potter', ()=>{
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login')
+    cy.get('.borderM > :nth-child(1) > .btn').click()
+    cy.get('#userSelect').select("Harry Potter")
+    cy.get('form.ng-valid > .btn').click()
+    cy.get('[ng-class="btnClass2"]').click()
+    cy.get('.form-control').type(100)
+    cy.get('form.ng-dirty > .btn').click()
+    cy.get('.error').should('contain.text', 'Deposit Successful')
+  
   })
+
+  it('Caso de teste: Fazer deposito de 200 reais usando o user do Ron Weasly e querer saca mais do que tem', ()=>{
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login')
+    cy.get('.borderM > :nth-child(1) > .btn').click()
+    cy.get('#userSelect').select("Ron Weasly")
+    cy.get('form.ng-valid > .btn').click()
+    cy.get('[ng-class="btnClass2"]').click()
+    cy.get('.form-control').type(200)
+    cy.get('form.ng-dirty > .btn').click()
+    cy.get('[ng-class="btnClass3"]').click()
+    cy.get('.form-control').type(300)
+    cy.get('form.ng-dirty > .btn').click
+    cy.get('.error').should('contain.text', 'Transaction Failed. You can not withdraw amount more than the balance.')
+  
+  })
+
 })
+
