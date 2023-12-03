d323aa23f8bb5ae142169ad35adb345ea70e28a0
annac@NITRO_ACR <annaclararenno@gmail.com>
2023-12-02 22:50:59 -0300

    Criando o teste negativo no site 'GLOBALSQA', depositando uma quantia e sacando mais do que depositado

--- prova_cypress/cypress/e2e/example1.cy.js
+++ prova_cypress/cypress/e2e/example1.cy.js
@@ -1,9 +1,9 @@
 
 /// <reference types = "cypress"/>
 
-describe('Criando cenario de teste para o site globalsqa',()=>{
+describe('Criando cenario de Interface para o site globalsqa',()=>{
 
-  it.skip('Caso de teste: Fazer deposito de 100 reais usando o user do Harry Potter', ()=>{
+  it('Caso de teste: Fazer deposito de 100 reais usando o user do Harry Potter', ()=>{
     cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login')
     cy.get('.borderM > :nth-child(1) > .btn').click()
     cy.get('#userSelect').select("Harry Potter")
@@ -23,9 +23,14 @@ describe('Criando cenario de teste para o site globalsqa',()=>{
     cy.get('[ng-class="btnClass2"]').click()
     cy.get('.form-control').type(200)
     cy.get('form.ng-dirty > .btn').click()
+    cy.wait(3000)
+    cy.get('.home').click()
+    cy.get('.borderM > :nth-child(1) > .btn').click()
+    cy.get('#userSelect').select("Ron Weasly")
+    cy.get('form.ng-valid > .btn').click()
     cy.get('[ng-class="btnClass3"]').click()
     cy.get('.form-control').type(300)
-    cy.get('form.ng-dirty > .btn').click
+    cy.get('form.ng-dirty > .btn').click()
     cy.get('.error').should('contain.text', 'Transaction Failed. You can not withdraw amount more than the balance.')
   
   })
